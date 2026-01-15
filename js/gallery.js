b5db66420fd1775ce66b61462a496728ba10328d
OskinSergey <sergey.oskin2019@yandex.ru>
2026-01-15 17:44:41 +0300

    Добавляет ДЗ "Открывается и закрывается"

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,12 @@
-const renderPitures = (photos) => {
+import { openFullSize } from "./full-picture.js";
+const renderPictures = (photos) => {
   const container = document.querySelector('.pictures');
   const fragment = document.createDocumentFragment();
   const template = document.querySelector('#picture');
 
   photos.forEach((photo) => {
     const pictureElement = template.content.cloneNode(true);
+    const link = pictureElement.querySelector('.picture');
     const img = pictureElement.querySelector('.picture__img');
     const likesElement = pictureElement.querySelector('.picture__likes');
     const commentsElement = pictureElement.querySelector('.picture__comments');
@@ -12,8 +14,12 @@ const renderPitures = (photos) => {
     img.alt = photo.description;
     likesElement.textContent = photo.likes;
     commentsElement.textContent = photo.comments.length;
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      openFullSize(photo);
+    });
     fragment.appendChild(pictureElement);
   });
   container.appendChild(fragment);
 };
-export {renderPitures};
+export { renderPictures };
